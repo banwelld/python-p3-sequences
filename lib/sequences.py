576cdcb584a70fddbc90bdc3abecbc394abcdeca
Dave Banwell <dave@davebanwell.com>
2024-12-15 21:10:04 -0500

    passes all tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+def fib_seq(length):
+    new_seq = []
+    for i in range(length):
+        element_value = i if i < 2 else sum(new_seq[-1:-3:-1])
+        new_seq.append(element_value)
+    return new_seq    
+
 def print_fibonacci(length):
-    pass
+    if not length:
+        print([])
+    else:
+        print(fib_seq(length))
